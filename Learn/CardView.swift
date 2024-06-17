f2c6906288ead533dabdf8e5719cb287ba5d310a
vinay Kumar ranjan <vinay.raj.apple@gmail.com>
2024-06-17 15:50:51 +0530

    Added image

--- Learn/CardView.swift
+++ Learn/CardView.swift
@@ -8,11 +8,56 @@
 import SwiftUI
 
 struct CardView: View {
+    
+    var colors = [Color("Color01"), Color("Color02")]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("developer-no1")
+            VStack{
+                Text("SwiftUI")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                Text("Better apps. Less code")
+                    .fontWeight(.light)
+                    .foregroundColor(Color.white)
+                    .italic()
+            } // Vstack
+            
+            .offset(y: -218)
+            
+            Button(action: {
+                print("Test")
+            }) {
+                HStack {
+                    Text("LEARN")
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    .accentColor(.white)
+                    Image(systemName: "arrow.right.circle")
+                        .font(Font.title.weight(.medium))
+                        .accentColor(.white)
+                }
+                .padding()
+                .background(LinearGradient(colors: colors, startPoint: .leading, endPoint: .trailing))
+                .clipShape(Capsule())
+                .shadow(color: Color("ColorShadow"), radius: 6, x: 0, y: 3)
+            }
+            .offset(y: 210)
+        }
+        .frame(width: 335, height: 545)
+        .background(LinearGradient(colors: colors, startPoint: .top, endPoint: .bottom))
+        .cornerRadius(16)
+        .shadow(radius: 8)
     }
 }
 
-#Preview {
-    CardView()
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+        
+    }
 }
